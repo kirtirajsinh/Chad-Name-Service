4177ab921fffd9fa03a5a7a876575b1ed7aac8e0
kirtirajsinh <kirtiraj3738@gmail.com>
2022-03-02 23:36:17 +0530

    Add functions to contract

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -14,6 +14,7 @@ contract Domains is ERC721URIStorage{
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    address payable public owner;
 
     string public tld;
 
@@ -22,9 +23,11 @@ contract Domains is ERC721URIStorage{
 
     mapping(string => address) domains;
     mapping(string => string) public records;
+    mapping(uint => string) public names;
     
     
     constructor(string memory _tld) payable ERC721("Chad Name Service", "CNS") {
+        owner = payable(msg.sender);
         tld = _tld;
         console.log("%s name service deployed", _tld);
     }
@@ -84,7 +87,7 @@ contract Domains is ERC721URIStorage{
     _safeMint(msg.sender, newRecordId);
     _setTokenURI(newRecordId, finalTokenUri);
     domains[name] = msg.sender;
-
+    names[newRecordId] = name;
     _tokenIds.increment();
   }
 
@@ -103,6 +106,35 @@ contract Domains is ERC721URIStorage{
     function getRecord(string calldata name) public view returns(string memory){
         return records[name];
     }
+
+    function getAllNames() public view returns(string[] memory){
+      console.log("getting all names from Contract");
+      string[] memory allNames = new string[](_tokenIds.current());
+      for(uint i = 0; i < _tokenIds.current(); i++){
+        allNames[i] = names[i];
+        console.log("Name for token %d is %s", i, allNames[i]);
+      }
+      return allNames;
+    }
+    function valid(string calldata name) public pure returns(bool) {
+  return StringUtils.strlen(name) >= 3 && StringUtils.strlen(name) <= 10;
+}
+
+    modifier onlyOwner() {
+      require(isOwner()); 
+      _;
+    }
+
+    function isOwner() public view returns (bool){
+      return msg.sender == owner;
+    }
+
+    function withdraw() public onlyOwner{
+      uint amount = address(this).balance;
+      (bool success, ) = msg.sender.call{value: amount}("");
+      require(success, "Failed to withdraw Matic");
+
+    } 
      
 
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,20 +1,34 @@
 const main = async () =>{
+    const [owner, superCoder] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains')
     const domainContract = await domainContractFactory.deploy("chad");
     await domainContract.deployed();
     console.log("Contract deployed to:", domainContract.address);
 
-    const txn = await domainContract.register("doom", {value: hre.ethers.utils.parseEther('0.5')})
+    let txn = await domainContract.register("a16z", {value: hre.ethers.utils.parseEther('0.5')})
     await txn.wait();
-
-    const address = await domainContract.getAddress("doom");
-    console.log("owner of doom:", address);
-
-    const domainOwner = await domainContract.getAddress("doom");
-    console.log("owner of domain: ", domainOwner)
 
     const balance = await hre.ethers.provider.getBalance(domainContract.address);
     console.log("balance: ", hre.ethers.utils.formatEther(balance));
+
+    try{
+        txn = await domainContract.connect(superCoder).withdraw();
+        await txn.wait();
+    }catch(error){
+        console.log("Could not rob contract");
+    }
+
+    let ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+    console.log("Balance of owner before WithDrawal", hre.ethers.utils.formatEther(ownerBalance))
+
+    txn = await domainContract.connect(owner).withdraw();
+    await txn.wait();
+
+    const contractBalance = await hre.ethers.provider.getBalance(domainContract.address);
+    ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+
+  console.log("Contract balance after withdrawal:", hre.ethers.utils.formatEther(contractBalance));
+  console.log("Balance of owner after withdrawal:", hre.ethers.utils.formatEther(ownerBalance));
 }
 
 const runMain = async () =>{
