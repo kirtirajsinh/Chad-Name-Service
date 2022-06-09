8a1d2ad6396ca8277778d9a48d67bc63d8039e59
kirtirajsinh <kirtiraj3738@gmail.com>
2022-06-09 15:40:32 +0530

    Add the frontend

--- scripts/run.js
+++ scripts/run.js
@@ -5,15 +5,16 @@ const main = async () =>{
     await domainContract.deployed();
     console.log("Contract deployed to:", domainContract.address);
 
-    let txn = await domainContract.register("a16z", {value: hre.ethers.utils.parseEther('0.5')})
+    let txn = await domainContract.register("a16z", {value: hre.ethers.utils.parseEther('0.00001')})
     await txn.wait();
 
     const balance = await hre.ethers.provider.getBalance(domainContract.address);
     console.log("balance: ", hre.ethers.utils.formatEther(balance));
 
     try{
-        txn = await domainContract.connect(superCoder).withdraw();
+        txn = await domainContract.connect(owner).withdraw();
         await txn.wait();
+        console.log("got the G's")
     }catch(error){
         console.log("Could not rob contract");
     }
@@ -26,6 +27,7 @@ const main = async () =>{
 
     const contractBalance = await hre.ethers.provider.getBalance(domainContract.address);
     ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+
 
   console.log("Contract balance after withdrawal:", hre.ethers.utils.formatEther(contractBalance));
   console.log("Balance of owner after withdrawal:", hre.ethers.utils.formatEther(ownerBalance));
